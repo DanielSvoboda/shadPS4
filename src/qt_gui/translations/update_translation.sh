bf15511670f466ca3d82630d4343c63ad0fa5a8f
DanielSvoboda <daniel.svoboda@hotmail.com>
2025-02-13 01:48:56 -0300

    Update update_translation.sh

--- src/qt_gui/translations/update_translation.sh
+++ src/qt_gui/translations/update_translation.sh
@@ -6,7 +6,7 @@ OPTS="-tr-function-alias QT_TRANSLATE_NOOP+=TRANSLATE,QT_TRANSLATE_NOOP+=TRANSLA
 SRCDIRS=$(realpath "$SCRIPTDIR/..") $(realpath "$SCRIPTDIR/../..")
 OUTDIR=$(realpath "$SCRIPTDIR")
 
-lupdate $SRCDIRS $OPTS -locations none -source-language en -ts "$OUTDIR/en_US.ts"
+lupdate $SRCDIRS $OPTS -locations none -source-language en_US  -ts "$OUTDIR/en_US.ts"
 
 if ! head -n 2 "$OUTDIR/en_US.ts" | grep -q "SPDX-FileCopyrightText"; then
     sed -i '2i\<!-- SPDX-FileCopyrightText: Copyright 2025 shadPS4 Emulator Project\n     SPDX-License-Identifier: GPL-2.0-or-later -->' "$OUTDIR/en_US.ts"
